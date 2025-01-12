812e701a67bc0a39d5b96afbad5663b6885caa01
Artyom Sokolov <15012003artem@mail.ru>
2025-01-12 15:25:41 +0500

    добавил возможность скачивать резюме

---
+++ download_resumes_xlsx.js
@@ -0,0 +1,45 @@
+const downloadVananciesButton = document.getElementById('download_resumes_btn');
+const keyWordsList = document.getElementById('keyWordsList');
+const regionsList = document.getElementById('regionsList');
+
+
+downloadVananciesButton.addEventListener('click', async () => {
+    const keyWords = getValuesFromUl(keyWordsList)
+    const regions = getValuesFromUl(regionsList)
+
+    const keyWordsParam = keyWords.join(',');;
+    const regionsParam = regions.join(',');;
+    
+    // Формируем URL с параметрами
+    const url = `https://localhost:44356/api/Resume/parse?keyWords=${keyWordsParam}&regions=${regionsParam}`;
+
+    // Открываем ссылку в новой вкладке
+    window.open(url, '_blank');
+});
+
+
+
+// Получение элементов из списка ul
+function getValuesFromUl(ul) {
+    const listItems = ul.querySelectorAll("li"); // Находим все элементы <li> внутри <ul>
+    const values = Array.from(listItems).map(item => item.textContent.trim());
+    return values
+}
+
+
+//Получение выбранного значения из радио кнопок
+function getSelectedMonthValue() {
+    const radioButtons = document.querySelectorAll('input[name="month_item"]');
+    let selectedValue = null;
+    
+    radioButtons.forEach(radio => {
+        if (radio.checked) {
+            selectedValue = radio.value; // Получаем value выбранной радио-кнопки
+        }
+    });
+
+    return selectedValue;
+}
+
+
+
